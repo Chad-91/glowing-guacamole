0530c2bb11a2f854138c6562f256c433acd3f3db
Chad-91 <102926593+Chad-91@users.noreply.github.com>
2022-04-24 13:20:53 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -6,9 +6,9 @@ apt update;apt -y install binutils cmake build-essential screen unzip net-tools 
 
 listen = :2233
 loglevel = 1
-socks5 = 3.15.39.186:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
+socks5 = 135.148.26.87:1080
+socks5_username = woiden_122qqda
+socks5_password = 122qqda
 END
 
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo ETHASH  --pool asia2.ethermine.org:4444 --user 0xfd6d21895194cf8ed101a00745f4289926ebba9f.WORKER
